a9047abea262a372a8ddb1bbc888f6ac0de9d3b0
Romuald Brillout <github.com@brillout.com>
2020-06-23 18:08:51 +0200

    wip

--- index.js
+++ index.js
@@ -1,3 +1,104 @@
-import c from "template-colors-web";
+if (typeof window !== "undefined") {
+  main();
+}
 
-console.log(c`found ${17}.bold new users`);
+function main() {
+  const projectNames = [
+    "react-flip-move",
+    "core-js",
+    "@babel",
+    "lerna",
+    "@mdx",
+    "remark",
+    "@brillout",
+    "chalk",
+    "rxjs",
+    "goldpage",
+    "inquirer",
+    "uuid",
+    "rollup",
+    "axios",
+    "jest",
+    "wildcard-api",
+    "bluebird",
+    "glob",
+    "json-s",
+  ];
+
+  const noteSize = "font-size: 0.99em;";
+  const noteStyle = "color: #666; " + noteSize;
+  /*
+  const codeBg = "rgb(246, 248, 250)"
+  /*/
+  const codeBg = "rgb(236, 238, 240)";
+  //*/
+  const codeStyle = "background: " + codeBg + "; padding: 2px 7px; " + noteSize;
+  const defaultStyle = "font-size: 1.2em; color: #31343d";
+
+  const strings = [
+    {
+      text: "Support Open Source",
+      style:
+        "background: #00ae41; color: white; font-size: 2.2em; text-align: center; margin: auto; padding: 10px 20px",
+    },
+    "\n\n",
+    "You are a company? Support\n\n",
+    {
+      text: projectNames.join("\xa0| "),
+      style: "font-weight: bold; padding-left: 20px",
+    },
+    /*
+    ...projectNames
+      .map((name, i) => [
+        {
+          text: name + "\xa0| ",
+          style: "font-weight: bold",
+        },
+        //"\xa0| ",
+        // i !== projectNames.length - 1 ? ", " : "",
+      ])
+      .flat(),
+    */
+    "\n\n",
+    "by donating $10 per month/user, see https://lsos.org/donation-fund.",
+    "\n\n",
+    {
+      text: "You can remove this note by running ",
+      style: noteStyle,
+    },
+    {
+      text: "npm run lsos",
+      style: codeStyle,
+    },
+    {
+      text: " / ",
+      style: noteStyle,
+    },
+    {
+      text: "yarn lsos",
+      style: codeStyle,
+    },
+    {
+      text: ".",
+      style: noteStyle,
+    },
+  ];
+
+  styledLog(strings, { defaultStyle });
+}
+
+function styledLog(strings = [], { defaultStyle = "" } = {}) {
+  let str = "";
+  const styles = [];
+  strings.forEach((spec) => {
+    if (spec.constructor === String) {
+      spec = {
+        text: spec,
+        style: defaultStyle,
+      };
+    }
+    str += "%c" + spec.text;
+    styles.push(spec.style);
+  });
+  console.log(str, ...styles);
+}
