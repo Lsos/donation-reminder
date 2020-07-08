282f5c0b3cce8ca8828f571b9ad5ada7b30f1b68
Romuald Brillout <github.com@brillout.com>
2020-07-08 23:56:06 +0200

    improve design

--- index.ts
+++ index.ts
@@ -4,6 +4,16 @@ import assert from "assert";
 const COLOR_GREEN = "#00ae41";
 const UNAVAILABLE_FUNDING_DEPS = Symbol();
 
+const MARGIN_LEFT = 20;
+const PROJECT_LOGO_SIZE = 14;
+const PROJECT_LOGO_MARGIN_RIGHT = 10;
+const INNER_MARGIN_SIZE =
+  MARGIN_LEFT + PROJECT_LOGO_SIZE + PROJECT_LOGO_MARGIN_RIGHT;
+const innerMarginStyle = [
+  "margin-left: " + INNER_MARGIN_SIZE + "px",
+  "margin-right: -" + INNER_MARGIN_SIZE + "px",
+];
+
 if (!skip()) {
   showDonationReminder();
 }
@@ -18,14 +28,24 @@ function showDonationReminder() {
     "by donating $10 per developer/month.",
     "\n\n",
     */
-    " ",
-    icon("https://lsos.org/logo.hash_b4859b66bf49915e8d8ea777e776cc50.svg"),
-    "Lsos Donation Fund",
-    separator(),
-    "Support all your open source dependencies",
-    separator(),
-    "https://lsos.org/fund/donate.",
-    "\n\n",
+    ...projectLine({
+      iconUrl: "https://i.imgur.com/5OtGNl2.png",
+      title: "React Table",
+      desc: [
+        "G'day ",
+        icon("https://discord.com/assets/df7ba0f4020ca70048a0226d1dfa73f6.svg"),
+        ", I'm Tanner, I'd love to be able to work full-time on open source and I'd appreciate any help! ",
+        icon("https://discord.com/assets/da3651e59d6006dfa5fa07ec3102d1f3.svg"),
+      ],
+      link: "https://lsos.org/npm/react-table",
+    }),
+    ...projectLine({
+      iconUrl:
+        "https://lsos.org/logo.hash_b4859b66bf49915e8d8ea777e776cc50.svg",
+      title: "Lsos Donation Fund",
+      desc: ["Support each of your open source dependencies"],
+      link: "https://lsos.org/fund/donate",
+    }),
     ...getNote(),
     "\n\n",
     ...getFooter(),
@@ -34,15 +54,57 @@ function showDonationReminder() {
   styledLog(strings, { defaultStyle: defaultStyle() });
 }
 
-function icon(iconUrl: string) {
+function projectLine({ iconUrl, title, desc, link }) {
+  const projectLogo = icon(iconUrl, {
+    size: PROJECT_LOGO_SIZE,
+    marginRight: 10,
+  });
+  projectLogo.style.push(
+    ...["margin-left: " + MARGIN_LEFT + "px", innerMarginStyle[1]]
+  );
+  return [
+    projectLogo,
+    {
+      text: title,
+      style: innerMarginStyle,
+    },
+    separator(),
+    ...desc.map((strings) => {
+      if (strings.constructor === String) {
+        strings = {
+          text: strings,
+          style: [],
+        };
+      }
+      strings.style.push(...innerMarginStyle);
+      return strings;
+    }),
+    separator(),
+    {
+      text: link,
+      style: innerMarginStyle,
+    },
+    "\n\n",
+  ];
+}
+
+function icon(
+  iconUrl: string,
+  { size = 10, marginRight = 0 }: { size?: number; marginRight?: number } = {}
+) {
   return {
     text: " ",
     style: [
       'background-image: url("' + iconUrl + '")',
       "background-size: contain",
+      "vertical-align: middle",
       "background-repeat: no-repeat",
-      "padding: 14px",
-      "line-height: 0px",
+      "padding-top: " + size + "px",
+      "padding-bottom: " + size + "px",
+      "padding-left: " + size + "px",
+      "padding-right: " + (size - 4) + "px",
+      "line-height: 0.6em",
+      "margin-right: " + marginRight + "px",
     ],
   };
 }
@@ -50,7 +112,7 @@ function icon(iconUrl: string) {
 function separator() {
   return {
     text: " | ",
-    style: [...defaultStyle(), "color: #888"],
+    style: [...defaultStyle(), ...innerMarginStyle, "color: #888"],
   };
 }
 
