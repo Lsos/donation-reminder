985b4d3bce4cb20d62b283fbe86ed101adaffcb3
Romuald Brillout <github.com@brillout.com>
2020-07-13 17:59:33 +0200

    wip

--- demo/index.js
+++ demo/index.js
@@ -1,1 +1,9 @@
-import "lsos";
+import { lsosDonationFund } from "@lsos/donation-fund";
+
+lsosDonationFund({
+  npmName: "@wildcard-api",
+  projectName: "Wildcard API",
+  //text: "G'day :wave:, I'm Romuald, I'd love to be able to work full-time on open source :slight_smile:",
+  text:
+    "Bonjour :wave:, I'm Romuald, your support would mean a lot to me :heart:",
+});
---
+++ src/skipDonationReminder.ts
@@ -0,0 +1,43 @@
+// The donation-reminder is shown only under certain conditions:
+// - The donation-reminder is only shown in the browser developer console.
+// - The donation-reminder is only shown in chromium based browsers (Chrome, Edge Chromium, Opera, etc.).
+// - The donation-reminder is not shown in staging nor production.
+
+export { skipDonationReminder };
+
+function skipDonationReminder() {
+  return !isBrowser() || !isChromium() || !isDev();
+}
+
+function isBrowser() {
+  return typeof window !== "undefined";
+}
+
+function isDev() {
+  if (!window?.process?.env) {
+    return true;
+  }
+  return ["", "dev", "development"].includes(process.env.NODE_ENV);
+}
+
+declare global {
+  interface Window {
+    opr: any;
+    opera: any;
+    chrome: any;
+  }
+}
+function isChromium() {
+  // https://stackoverflow.com/questions/57660234/how-can-i-check-if-a-browser-is-chromium-based
+  // https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
+
+  const isOpera =
+    (!!window.opr && !!window.opr.addons) ||
+    !!window.opera ||
+    navigator.userAgent.indexOf(" OPR/") >= 0;
+
+  // Also detects Edge Chromium
+  const isChrome = !!window.chrome;
+
+  return isOpera || isChrome;
+}
