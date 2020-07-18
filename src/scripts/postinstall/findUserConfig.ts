a3cb424bb9c47d9af038632da4ef7914e0c03ea0
Romuald Brillout <github.com@brillout.com>
2020-07-18 12:29:45 +0200

    wip

--- src/scripts/postinstall/findUserConfig.ts
+++ src/scripts/postinstall/findUserConfig.ts
@@ -6,13 +6,20 @@ export { findUserConfig };
 
 function findUserConfig() {
   const userConfig = UserConfig.get();
-  const isRemoved = userConfig?.donationReminder?.remove;
-  // @ts-ignore
-  assert({}?.doesntExist === null);
+  const isRemoved = userConfig?.donationReminder?.remove ?? null;
+  assert_ts_syntax();
   assert([null, true, false].includes(isRemoved));
   replaceFileContent(
     require.resolve("../../env/isRemoved.js"),
     "isRemoved",
     isRemoved
   );
+}
+
+function assert_ts_syntax() {
+  // @ts-ignore
+  assert({}?.doesntExist === undefined);
+  // @ts-ignore
+  assert({}?.doesntExist ?? null === null);
+  assert({ doesntExist: 42 }?.doesntExist ?? null === 42);
 }
